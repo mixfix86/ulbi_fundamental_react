4af33518affe93070bff4792c5006e9a97c0afbb
max <mixfix@gmail.com>
2023-03-16 21:27:31 +0500

    remembered setState

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import { useState } from 'react';
+import { Counter } from './components/Counter';
 
 export const App = () => {
+  const [value, setValue] = useState('')
 
-  let [count, setCount] = useState(0)
-  const increment = () => {
-    setCount(prev => prev + 1)
-  }
-  const decrement = () => {
-    setCount(prev => prev - 1)
-  }
   return (
     <div>
-      <h1>{count}</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <h1>{value}</h1>
+      <input type="text" name="input" onChange={(e) => setValue(e.target.value)} value={value} />
+      <Counter />
     </div>
   );
 }
---
+++ src/components/Counter.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+export const Counter = () => {
+  const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount(prev => prev + 1)
+  }
+
+  const decrement = () => {
+    setCount(prev => prev - 1)
+  }
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  )
+}
