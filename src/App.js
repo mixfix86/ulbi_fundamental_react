5bb9b9fa4cb9ffc67aa29acd9b41678c40ba2258
max <mixfix@gmail.com>
2023-03-23 22:15:37 +0500

    add users hook

--- src/App.js
+++ src/App.js
@@ -1,42 +1,30 @@
-import React from 'react';
-import { useState, useMemo } from 'react';
-import { PostList } from './components/PostList/PostList';
-import { PostForm } from './components/PostForm/PostForm';
+import React from "react";
+import { useState } from "react";
+import { PostList } from "./components/PostList/PostList";
+import { PostForm } from "./components/PostForm/PostForm";
 
-import './App.css';
-import { PostFilter } from './components/PostFilter/PostFilter';
-import { MyModal } from './components/MyModal/MyModal';
-import { MyButton } from './components/UI/button/MyButton';
+import "./App.css";
+import { PostFilter } from "./components/PostFilter/PostFilter";
+import { MyModal } from "./components/MyModal/MyModal";
+import { MyButton } from "./components/UI/button/MyButton";
+import { usePosts, useSortedposts } from "./hooks/usePosts";
 
 export const App = () => {
   const [posts, setPosts] = useState([
-    { id: 1, title: 'JS', body: 'JS - programming language' },
-    { id: 2, title: 'C', body: 'C - programming language' },
-    { id: 3, title: 'C#', body: 'C# - programming language' },
-    { id: 4, title: 'JAVA', body: 'JAVA - programming language' },
+    { id: 1, title: "JS", body: "JS - programming language" },
+    { id: 2, title: "C", body: "C - programming language" },
+    { id: 3, title: "C#", body: "C# - programming language" },
+    { id: 4, title: "JAVA", body: "JAVA - programming language" },
   ]);
 
-  const [filter, setFilter] = useState({ query: '', sort: '' });
+  const [filter, setFilter] = useState({ query: "", sort: "" });
   const [modalVisible, setModalVisible] = useState(false);
 
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) =>
-        a[filter.sort].localeCompare(b[filter.sort])
-      );
-    }
-    return posts;
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchPosts = useMemo(() => {
-    return sortedPosts.filter((post) =>
-      post.title.toLowerCase().includes(filter.query)
-    );
-  }, [filter.query, sortedPosts]);
+  const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
-    setModalVisible(false)
+    setModalVisible(false);
   };
 
   const deletePost = (post) => {
@@ -44,9 +32,9 @@ export const App = () => {
   };
 
   return (
-    <div className='App'>
+    <div className="App">
       <MyButton
-        style={{ marginTop: '15px' }}
+        style={{ marginTop: "15px" }}
         onClick={() => setModalVisible(true)}
       >
         Добавить пост
@@ -56,13 +44,13 @@ export const App = () => {
       </MyModal>
 
       <div>
-        <hr style={{ margin: '15px 0' }} />
+        <hr style={{ margin: "15px 0" }} />
         <PostFilter filter={filter} setFilter={setFilter} />
       </div>
       <PostList
         remove={deletePost}
         posts={sortedAndSearchPosts}
-        title='Список постов'
+        title="Список постов"
       />
     </div>
   );
