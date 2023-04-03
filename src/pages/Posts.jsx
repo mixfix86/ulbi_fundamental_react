67ecab89eb590439cd1caca9e4c1bd65eb71c067
mix <mixfix@gmail.com>
2023-04-03 23:50:24 +0500

    finish

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
+import PostService from '../API/PostService';
 import { useState } from 'react';
 import { PostList } from '../components/PostList/PostList';
 import { PostForm } from '../components/PostForm/PostForm';
@@ -6,12 +7,9 @@ import { PostFilter } from '../components/PostFilter/PostFilter';
 import { MyModal } from '../components/UI/MyModal/MyModal';
 import { MyButton } from '../components/UI/button/MyButton';
 import { usePosts } from '../hooks/usePosts';
-import PostService from '../API/PostService';
-
 import { Loader } from '../components/UI/Loader/Loader';
 import { useFetching } from '../hooks/useFetching';
 import { getPageCount } from '../utils/pages';
-
 import { Pagination } from '../components/UI/pagination/Pagination';
 
 export const Posts = () => {
@@ -21,10 +19,12 @@ export const Posts = () => {
   const [totalPages, setTotalPages] = useState(0);
   const [limit, setLimit] = useState(10);
   const [page, setPage] = useState(1);
+  const lastElem = useRef();
+  const observer = useRef();
 
   const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
     const response = await PostService.getAll(limit, page);
-    setPosts(response.data);
+    setPosts([...posts, ...response.data]);
     const totalCount = response.headers['x-total-count'];
     setTotalPages(getPageCount(totalCount, limit));
   });
@@ -32,7 +32,22 @@ export const Posts = () => {
   const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query);
 
   useEffect(() => {
-    fetchPosts();
+    console.log(isPostsLoading);
+    if (isPostsLoading) return;
+    if (observer.current) observer.current.disconnect();
+    let callback = function (entries, observer) {
+      if (entries[0].isIntersecting && page < totalPages) {
+        console.log(page);
+        console.log(totalPages);
+        setPage(page => page + 1);
+      }
+    };
+    observer.current = new IntersectionObserver(callback);
+    observer.current.observe(lastElem.current);
+  }, [isPostsLoading]);
+
+  useEffect(() => {
+    fetchPosts(limit, page);
   }, [page]);
 
   const createPost = (newPost) => {
@@ -67,7 +82,7 @@ export const Posts = () => {
         <PostFilter filter={filter} setFilter={setFilter} />
       </div>
       {postError ?? <h1>Что-то пошло не так...</h1>}
-      {isPostsLoading ? (
+      {isPostsLoading && (
         <div
           style={{
             display: 'flex',
@@ -77,13 +92,15 @@ export const Posts = () => {
         >
           <Loader />
         </div>
-      ) : (
-        <PostList
-          remove={deletePost}
-          posts={sortedAndSearchPosts}
-          title='Список постов'
-        />
       )}
+      <PostList
+        remove={deletePost}
+        posts={sortedAndSearchPosts}
+        title='Список постов'
+      />
+
+      <div ref={lastElem} style={{ background: 'red', height: '5px' }}></div>
+
       <Pagination totalPages={totalPages} page={page} changePage={changePage} />
     </div>
   );
